9cfe838a459e3393b3a6bb2d788ee09b2a03ec70
uriahpissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-05-26 22:04:55 -0400

    Add files via upload

---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        i = 0
+        while num != 0:
+            if num % 2 == 0:
+                num = num/2
+                i += 1
+            else:
+                num -= 1
+                i += 1
+        return i
+        
