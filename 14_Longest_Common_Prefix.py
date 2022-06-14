1320a876748fd27c4409fe285d5928b380d60b40
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-06-14 15:00:53 -0400

    Add files via upload

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pre = strs[0]
+        for i in strs:
+            while not i.startswith(pre):
+                pre = pre[:-1]
+        return pre  
