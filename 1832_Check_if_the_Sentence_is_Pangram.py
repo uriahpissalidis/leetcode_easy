baa19b04f8013857f1c6a46e0e08f2a6a1fb3bbf
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-06-25 14:34:24 -0400

    Create 1832_Check_if_the_Sentence_is_Pangram.py

---
+++ 1832_Check_if_the_Sentence_is_Pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        seen = set()
+        for char in sentence:
+            seen.add(char)
+        if len(seen) == 26: return True
+        else: return False
