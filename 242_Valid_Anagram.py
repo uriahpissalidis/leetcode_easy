127720b69d009957d434863f7a65ea33fee79196
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-09-14 17:41:06 -0400

    Update 242_Valid_Anagram.py

--- 242_Valid_Anagram.py
+++ 242_Valid_Anagram.py
@@ -1,3 +1,25 @@
+# second solution, more recent
+# Runtime: O(n)
+# Memory: O(n) because of the counter
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d = Counter(s)
+        for letter in t:
+            if letter in d:
+                d[letter] -= 1
+        for key in d:
+            if d[key] != 0:
+                return False
+        return True
+
+"""
+Another solution
+# Runtime: O(n)
+# Memory: O(n)
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -6,3 +28,4 @@ class Solution:
             if s.count(i) != t.count(i):
                 return False
         return True
+"""
