2428c478a3d8c5dca103ba9cb0a2eda8dddad570
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-10-01 20:26:45 -0400

    Update 191_Number_of_1_Bits.py

--- 191_Number_of_1_Bits.py
+++ 191_Number_of_1_Bits.py
@@ -1,3 +1,17 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
+    def hammingWeight(self, n: int) -> int: 
+        # O(n) runtime, O(1) space
+        ans = 0
+        while n:
+            n &= (n-1) # removes the rightmost bit
+            ans += 1
+        return ans
+        
+        # O(n) runtime, O(1) space
+        ans = 0
+        while (n != 0):
+            ans = ans + (n & 1) # removes the rightmost bit
+            n = n >> 1
+        return ans
+    
         return str(bin(n)).count("1")
