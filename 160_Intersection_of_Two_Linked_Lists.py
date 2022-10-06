6c82f0728579db2eb5c4065180b8d5223d31c920
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-10-06 16:47:55 -0400

    Create 160_Intersection_of_Two_Linked_Lists.py

---
+++ 160_Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        h1, h2 = headA, headB
+        while h1 != h2:
+            if not h1:
+                h1 = headB
+            else:
+                h1 = h1.next
+            if not h2:
+                h2 = headA
+            else:
+                h2 = h2.next
+        return h1
