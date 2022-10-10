448f071d7434065805451f208f59439ec52d0422
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-10-09 20:44:13 -0400

    Update 461_Hamming_Distance.py

--- 461_Hamming_Distance.py
+++ 461_Hamming_Distance.py
@@ -15,3 +15,14 @@ class Solution:
             y += 1
             x = x & (x - 1)
         return y
+    
+        # NO bit manipulation solution
+        # this step is for padding 0's
+        longer = len(bin(y))-2
+        if x > y: longer = len(bin(x))-2
+            
+        # turn both to binary, count 1's in binary representation within count
+        x, y, count = format(x, '0b').zfill(longer), format(y, '0b').zfill(longer), 0 
+        for i in range(len(x)):
+            if x[i] != y[i]: count += 1
+        return count
