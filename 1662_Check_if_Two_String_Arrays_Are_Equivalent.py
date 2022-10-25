4124b235feae3d4a19cad74ab0cc81cb50e79a04
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-10-25 13:50:00 -0400

    Update 1662_Check_if_Two_String_Arrays_Are_Equivalent.py

--- 1662_Check_if_Two_String_Arrays_Are_Equivalent.py
+++ 1662_Check_if_Two_String_Arrays_Are_Equivalent.py
@@ -6,3 +6,17 @@ class Solution:
             return True
         else:
             return False
+        
+        # cleaner solution
+        return "".join(word1)=="".join(word2)
+        
+        # lengthier, utilizing string concatenation
+        piece1, piece2 = '', ''
+        for piece in word1:
+            piece1 += piece
+        for piece in word2:
+            piece2 += piece
+        
+        if piece1 == piece2:
+            return True
+        return False
