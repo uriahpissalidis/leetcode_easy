2c9399332b5bc5ef1ac2727c7981269b9b56181f
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-12-08 16:54:28 -0500

    Update 1207_Unique_Number_of_Occurrences.py

--- 1207_Unique_Number_of_Occurrences.py
+++ 1207_Unique_Number_of_Occurrences.py
@@ -2,3 +2,14 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         c = collections.Counter(arr)
         return len(set(c.values())) == len(set(c.keys()))
+        
+        # 2nd, faster solution
+        c = Counter(arr)
+        x = []
+        for k,v in c.items():
+            if v in x:
+                x.append(v)
+                return False
+            x.append(v)
+        
+        return True
